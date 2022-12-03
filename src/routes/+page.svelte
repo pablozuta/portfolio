c2b3e88f077991cccdc0735838e9cfa57b632d39
Pablo Zuta <pablo.zuta.h@gmail.com>
2022-12-03 13:03:47 -0400

    se crea pagina base y layout general

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,173 @@
-<h1>Pablo Zuta</h1>
-<h2>Front-End Developer</h2>
+<main>
+<div class="grid-6" style="padding-block:2%;">
+       
+    <div class="card">
+     <img src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" class="imagen-principal" alt="Portrait">
+     
+    </div>
+           
+            
+    <div class="card">
+     
+      <h3>Hi , I'm Pablo Zuta</h3>
+     <h4>I'm a web developer from Chile</h4>
+          
+     <p >As a web developer, I have a passion for creating user-friendly and visually appealing websites. My skillset includes HTML, CSS, JavaScript, and various frameworks and libraries such as React, Django and Svelte.
+
+        I have experience working on both front-end and back-end development, and I am always looking for ways to improve my skills and stay up-to-date with the latest industry trends.</p>
+     <button class="boton">Read More</button>
+
+     </div>
+
+
+ </div>
+</main>
+<!-- aca termina el card main-->
+
+<!--aca empieza el banner horizontal de skills-->
+ <div class="card-skills">
+    <h2>MY SKILLS</h2>
+    <div class="card-skills-badges">
+    <a href="https://www.djangoproject.com/" target="_blank" rel="noreferrer"> <img src="https://cdn.worldvectorlogo.com/logos/django.svg" alt="django" width="40" height="40"/> </a> <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"/> </a> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a> <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> <a href="https://pytorch.org/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/pytorch/pytorch-icon.svg" alt="pytorch" width="40" height="40"/> </a> <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/> </a> <a href="https://svelte.dev" target="_blank" rel="noreferrer"> <img src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg" alt="svelte" width="40" height="40"/> </a> 
+</div>
+ </div>
+ <!--aca termina el banner horizontal de skills-->
+
+ <!-- aca empieza el grid portfolio-->
+
+<center><h2>PORTFOLIO</h2></center>
+ 
+ <div class="grid-6" style="background-color:#efb229 ; padding-inline: 2%;">
+          
+    <div class="card">
+     <img src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" class="card-imagen-horizontal" alt="imagen of lucy Mckenzie">
+     <h4 class="card-portfolio-text-big">Magazine</h4><br>
+     
+     <p class="card-portfolio-text">McKenzie collaborates with other creatives regularly. Through her collaborations, she challenges the notion of authorship by pointing to the strength of collective actions. </p>
+    </div>
+           
+            
+    <div class="card">
+     <img src="https://images.unsplash.com/photo-1587440871875-191322ee64b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80" class="card-imagen-horizontal" alt="imagen of emily speed" >
+
+     <h4 class="card-portfolio-text-big">Emily Speed</h4><br>
+          
+     <p class="card-portfolio-text">Experience a new film installation by North West based artist Emily Speed. The film is inspired by Edwin Abbott’s 1884 novella Flatland, a satire of Victorian society, where all existence is limited to two dimensions and women are restricted to thin, straight lines. </p>
+     </div>
+
+     <div class="card">
+       <img src="https://images.unsplash.com/photo-1621839673705-6617adf9e890?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80" class="card-imagen-horizontal" alt="Zahoor ul Akhlaq painting">
+       <h4 class="card-portfolio-text-big">Zahoor ul Akhlaq</h4><br>
+       
+       <p class="card-portfolio-text">Such marks make the image oscillate back and forth, back into the process of miniature making and forward into a Modernist frame. The reverse gear refers to the initial stage of drawing exercises.</p>
+      </div>
+
+      <div class="card">
+       <img src="https://images.unsplash.com/photo-1522542550221-31fd19575a2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHdlYiUyMGRlc2lnbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" class="card-imagen-horizontal" alt="imagen of emily speed" >
+
+       <h4 class="card-portfolio-text-big">VALIS</h4><br>
+            
+       <p class="card-portfolio-text">And I love the kind of ongoing love affair, aesthetically and politically, between Black Americans and Puerto Ricans. There’s been so much synergy, especially in the Bronx, in East Harlem, Lower East Side, and parts of Brooklyn.</p>
+       </div>
+
+
+       <div class="card">
+         <img src="https://images.unsplash.com/photo-1517134191118-9d595e4c8c2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" class="card-imagen-horizontal" alt="imagen of lucy Mckenzie">
+         <h4 class="card-portfolio-text-big">Love Your Work</h4><br>
+         
+         <p class="card-portfolio-text">Can architecture and design write scenarios for the next generation of work? What will it take to anticipate and formulate work environments and experiences that are productive, humane, and ecologically responsible? </p>
+        </div>
+
+
+        <div class="card">
+         <img src="https://images.unsplash.com/photo-1497091071254-cc9b2ba7c48a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80" class="card-imagen-horizontal" alt="imagen of lucy Mckenzie">
+         <h4 class="card-portfolio-text-big">Harvard Design</h4><br>
+         
+         <p class="card-portfolio-text">Contemporary public space projects should address both the digital and the physical layer of a place. The two are becoming increasingly interconnected and dependent on each other. </p>
+        </div>
+
+ </div>
+ <center> <footer>
+    <p>&copy; 2022 Made with ❤️ by <strong>Pablo Zuta</strong> </p>
+ </footer></center>
+
+
+ 
+
+ <style>
+    .grid-6{
+    margin-top: 1rem;
+    display: grid;
+    grid-template-columns:repeat(auto-fit, minmax(21rem, 1fr)) ;
+    padding: 5%;
+    gap: 3rem;
+}
+
+.imagen-principal{
+    width: 100%;
+    aspect-ratio: 1/1;
+    object-fit: cover;
+    box-shadow:
+      0 1px 1px hsl(0deg 0% 0% / 0.075),
+      0 2px 2px hsl(0deg 0% 0% / 0.075),
+      0 4px 4px hsl(0deg 0% 0% / 0.075),
+      0 8px 8px hsl(0deg 0% 0% / 0.075),
+      0 16px 16px hsl(0deg 0% 0% / 0.075)
+    ;
+    border-radius: 3px;
+}
+.card-imagen-horizontal{
+    width: 100%;
+    aspect-ratio: 1.5/1.1;
+    object-fit: cover;
+}
+
+.boton {
+  background-color:#efb229;
+  border: none;
+  color: blanchedalmond;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 18px;
+  border-radius: 3px;
+  
+}
+
+.boton:hover{
+    color: black;
+    cursor: pointer;
+}
+
+.card-skills{
+    display: flex;
+    flex-direction: column;
+}
+
+.card-skills > h2{
+    text-align: center;
+}
+
+.card-skills-badges{
+    display: flex;
+    flex-direction: row;
+    width: 80%;
+    justify-content: space-between;
+    margin: auto;
+   
+   
+}
+
+.card-portfolio-text-big{
+    text-align: center;
+    font-size: 1.3rem;
+}
+
+.card-portfolio-text{
+    margin-top: -1rem;
+    text-align: center;
+}
+
+ </style>
 
