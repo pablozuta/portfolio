acb5f5a01331a39d6aab900a710dd2ffc5f6e2c6
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-03-10 15:46:24 -0400

    se acorta texto 'astro'

--- src/i18n.js
+++ src/i18n.js
@@ -1,70 +1,70 @@
-import { addMessages } from 'svelte-i18n'
+import { addMessages } from 'svelte-i18n';
 
 addMessages('en', {
-  greet: "Hi , I'm ",
-  dev: "I'm a Web Developer based in Chile",
-  have: 'I have a passion for technology and am always looking to ',
-  have2: 'learn and improve ',
-  have3:
-    'my skills. My skillset includes HTML, CSS, JavaScript and various frameworks and libraries such as React and Svelte. In my free time, I enjoy making music and exploring new ways to express myself creatively.',
-  readMore: 'Read More',
-  social:
-    'Social media App using React and Material UI , implementing functionalities such as shared layout , responsive design , reading from a database and light/dark mode themes.',
-  art: 'Art Magazine Website build with the Astro framework.',
-  crypto:
-    'Crypto Seach page using Svelte to connect to the Gecko Api to display values of different types of coins such as Bitcoin and Ethereum.',
-  typo: 'Typography page using materials from the California Institute of the Arts course "introduction to typography" , made with Svelte and images hosting on Cloudinary.',
-  blog: 'Go by Example website recreation using NextJS, Typescript and MDX.',
-  journey: 'THE JOURNEY',
-  j1: 'As a self-taught web developer, I have experienced both the rewards and challenges of learning this field on my own. It has required a lot of dedication and motivation to learn the various technologies and concepts, and there have been times when I have felt frustrated or overwhelmed. One of the biggest challenges was staying motivated and on track.',
-  j3: "I found it helpful to set small, achievable goals and practice my skills by building projects and websites. This gave me a sense of what it's like to work on a real-world project and also allowed me to show my work to potential employers or clients. Also connecting with other developers and joining online communities to get support, encouragement and ideas was helpful in building my skills.",
-  f1: ' Now I am willing to try new things and take on new challenges and I know that the best way to learn is to get hands-on experience. I am excited to see where my learning journey will take me next.',
-  courses: 'COURSES',
-  html: 'The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.',
-  css: 'Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML.',
-  javascript:
-    'JavaScript, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.',
-  git: 'Git is a distributed version control system designed to handle everything from small to very large projects with speed and efficiency.',
-  react:
-    'React is front-end JavaScript library for building user interfaces based on UI components.',
-  svelte:
-    'Svelte Framework is a compiler that generates minimal and highly optimized JavaScript code.',
-  python:
-    'Python is a programming language used successfully in thousands of real-world business applications around the world, including many large and mission critical systems.',
-  astro:
-    'Astro represent a leading paradigm shift for frontend web architecture. Astro extracts your UI into smaller, isolated components on the page. Unused JavaScript is replaced with lightweight HTML.',
-})
+	greet: "Hi , I'm ",
+	dev: "I'm a Web Developer based in Chile",
+	have: 'I have a passion for technology and am always looking to ',
+	have2: 'learn and improve ',
+	have3:
+		'my skills. My skillset includes HTML, CSS, JavaScript and various frameworks and libraries such as React and Svelte. In my free time, I enjoy making music and exploring new ways to express myself creatively.',
+	readMore: 'Read More',
+	social:
+		'Social media App using React and Material UI , implementing functionalities such as shared layout , responsive design , reading from a database and light/dark mode themes.',
+	art: 'Art Magazine Website build with the Astro framework.',
+	crypto:
+		'Crypto Seach page using Svelte to connect to the Gecko Api to display values of different types of coins such as Bitcoin and Ethereum.',
+	typo: 'Typography page using materials from the California Institute of the Arts course "introduction to typography" , made with Svelte and images hosting on Cloudinary.',
+	blog: 'Go by Example website recreation using NextJS, Typescript and MDX.',
+	journey: 'THE JOURNEY',
+	j1: 'As a self-taught web developer, I have experienced both the rewards and challenges of learning this field on my own. It has required a lot of dedication and motivation to learn the various technologies and concepts, and there have been times when I have felt frustrated or overwhelmed. One of the biggest challenges was staying motivated and on track.',
+	j3: "I found it helpful to set small, achievable goals and practice my skills by building projects and websites. This gave me a sense of what it's like to work on a real-world project and also allowed me to show my work to potential employers or clients. Also connecting with other developers and joining online communities to get support, encouragement and ideas was helpful in building my skills.",
+	f1: ' Now I am willing to try new things and take on new challenges and I know that the best way to learn is to get hands-on experience. I am excited to see where my learning journey will take me next.',
+	courses: 'COURSES',
+	html: 'The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.',
+	css: 'Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML.',
+	javascript:
+		'JavaScript, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.',
+	git: 'Git is a distributed version control system designed to handle everything from small to very large projects with speed and efficiency.',
+	react:
+		'React is front-end JavaScript library for building user interfaces based on UI components.',
+	svelte:
+		'Svelte Framework is a compiler that generates minimal and highly optimized JavaScript code.',
+	python:
+		'Python is a programming language used successfully in thousands of real-world business applications around the world, including many large and mission critical systems.',
+	astro:
+		'Astro represent a paradigm shift for frontend web architecture. Astro extracts your UI into smaller components on the page. Unused JavaScript is replaced with lightweight HTML.'
+});
 addMessages('es', {
-  greet: 'Hola, soy ',
-  dev: 'Soy un Desarrollador Web de Chile',
-  have: 'Soy apasionado por la tecnologia y siempre estoy buscando  ',
-  have2: 'aprender y mejorar',
-  have3:
-    'mis capacidades. Mi skillset incluye HTML, CSS, JavaScript y varios frameworks y librerias como React y Svelte.En mi tiempo libre disfruto creando musica y explorando nuevas maneras de expresarme creativamente.',
-  readMore: 'Leer Mas',
-  social:
-    'Red Social App usando React y Material UI, implementa funcionalidades como shared layouts , lectura desde base de datos , diseño responsivo y light/dark theme switch.',
-  art: 'Sitio web Revista de Arte construido con el framework Astro.',
-  crypto:
-    'Buscador de valores crypto usando Svelte para conectarse a la API Gecko y mostrar diferentes tipos de valores de cryptomonedas como Bitcoin y Ethereum.',
-  typo: 'Website Educacional sobre Tipografia que usa materiales del curso "Introduction to Typography" del California Institute of the Arts. Creado con Svelte y el servicio Cloudinary como alojamiento de imagenes.',
-  blog: 'Recreacion de pagina Go by Example usando NextJS, Typescript y MDX.',
-  journey: 'EL CAMINO',
-  j1: 'Como Desarrollador autodidacta eh experimentado tanto los exitos y las dificultades de aprender este campo por mi cuenta. A requerido un monton de dedicacion y motivacion para aprender las varias tecnologias y conceptos , en ocaciones me eh sentido frustrado o abrumado. Uno de los mayores desafios a sido mantener la motivacion y no perder el rumbo.',
-  j3: 'Algo que fue beneficioso fue ponerme metas que fueran pequeñas y a mi alcanze y practicar mis habilidades contruyendo proyectos y websites. Esto me dio un sentido de como es la dinamica de trabajo en un proyecto del mundo real y a la vez me permite mostrar mi trabajo a potenciales clientes y empleadores. Tambien, conectar con otros desarrolladores y unirme a comunidades online que brindan soporte, ideas y enseñanzas a sido beneficioso en seguir mejorando mis capacidades',
-  f1: 'En este momento estoy con ansias de probar cosas nuevas y tomar nuevos desafios , se que la mejor manera de aprender es con experiencia practica. Estoy expectante por ver hacia donde me llevara mi camino de aprendizaje en el tiempo que viene.',
-  courses: 'CURSOS',
-  html: 'El HyperText Markup Language o HTML es el lenguaje de marcado standard para documentos diseñados para ser mostrados en un navegador web.',
-  css: 'CSS (Cascading Style Sheets) es el lenguaje usado para describir la presentacion de un documento escrito en HTML o XML.',
-  javascript:
-    'Javascript, es un lenguaje de programacion que forma las tecnologias bases de la World Wide Web junto con HTML y CSS.',
-  git: 'Git es un sistema distribuido de control de versiones diseñado para procesar desde pequeños hasta enormes proyectos con velocidad y eficiencia.',
-  react:
-    'React es una libreria de JavaScript para construir interfaces de usuario basadas en componentes UI.',
-  svelte:
-    'Svelte es un framework que compila codigo Javascript de una manera minimalista y altamente optimizada.',
-  python:
-    'Python es un lenguaje de programacion usado exitosamente en miles de proyectos desde aplicaciones de negocios hasta data science y machine learning.',
-  astro:
-    'Astro representa un cambio de paradigma en la arquitectura del front-end. Astro extrae UI en pequeños componentes en la pagina. El JavaScript no usado es reemplazado por HTML ligero.',
-})
+	greet: 'Hola, soy ',
+	dev: 'Soy un Desarrollador Web de Chile',
+	have: 'Soy apasionado por la tecnologia y siempre estoy buscando  ',
+	have2: 'aprender y mejorar',
+	have3:
+		'mis capacidades. Mi skillset incluye HTML, CSS, JavaScript y varios frameworks y librerias como React y Svelte.En mi tiempo libre disfruto creando musica y explorando nuevas maneras de expresarme creativamente.',
+	readMore: 'Leer Mas',
+	social:
+		'Red Social App usando React y Material UI, implementa funcionalidades como shared layouts , lectura desde base de datos , diseño responsivo y light/dark theme switch.',
+	art: 'Sitio web Revista de Arte construido con el framework Astro.',
+	crypto:
+		'Buscador de valores crypto usando Svelte para conectarse a la API Gecko y mostrar diferentes tipos de valores de cryptomonedas como Bitcoin y Ethereum.',
+	typo: 'Website Educacional sobre Tipografia que usa materiales del curso "Introduction to Typography" del California Institute of the Arts. Creado con Svelte y el servicio Cloudinary como alojamiento de imagenes.',
+	blog: 'Recreacion de pagina Go by Example usando NextJS, Typescript y MDX.',
+	journey: 'EL CAMINO',
+	j1: 'Como Desarrollador autodidacta eh experimentado tanto los exitos y las dificultades de aprender este campo por mi cuenta. A requerido un monton de dedicacion y motivacion para aprender las varias tecnologias y conceptos , en ocaciones me eh sentido frustrado o abrumado. Uno de los mayores desafios a sido mantener la motivacion y no perder el rumbo.',
+	j3: 'Algo que fue beneficioso fue ponerme metas que fueran pequeñas y a mi alcanze y practicar mis habilidades contruyendo proyectos y websites. Esto me dio un sentido de como es la dinamica de trabajo en un proyecto del mundo real y a la vez me permite mostrar mi trabajo a potenciales clientes y empleadores. Tambien, conectar con otros desarrolladores y unirme a comunidades online que brindan soporte, ideas y enseñanzas a sido beneficioso en seguir mejorando mis capacidades',
+	f1: 'En este momento estoy con ansias de probar cosas nuevas y tomar nuevos desafios , se que la mejor manera de aprender es con experiencia practica. Estoy expectante por ver hacia donde me llevara mi camino de aprendizaje en el tiempo que viene.',
+	courses: 'CURSOS',
+	html: 'El HyperText Markup Language o HTML es el lenguaje de marcado standard para documentos diseñados para ser mostrados en un navegador web.',
+	css: 'CSS (Cascading Style Sheets) es el lenguaje usado para describir la presentacion de un documento escrito en HTML o XML.',
+	javascript:
+		'Javascript, es un lenguaje de programacion que forma las tecnologias bases de la World Wide Web junto con HTML y CSS.',
+	git: 'Git es un sistema distribuido de control de versiones diseñado para procesar desde pequeños hasta enormes proyectos con velocidad y eficiencia.',
+	react:
+		'React es una libreria de JavaScript para construir interfaces de usuario basadas en componentes UI.',
+	svelte:
+		'Svelte es un framework que compila codigo Javascript de una manera minimalista y altamente optimizada.',
+	python:
+		'Python es un lenguaje de programacion usado exitosamente en miles de proyectos desde aplicaciones de negocios hasta data science y machine learning.',
+	astro:
+		'Astro representa un cambio de paradigma en la arquitectura del front-end. Astro extrae UI en pequeños componentes y el JavaScript no usado es reemplazado por HTML.'
+});
